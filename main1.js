433ca24f8fea2607dcef033f3d0128baea1f6096
Alicia Duato <ad@polygravity.io>
2018-12-04 14:33:54 +0100

    This is a faliure

--- main1.js
+++ main1.js
@@ -1,6 +1,5 @@
 const SHA256 = require('crypto-js/sha256');
 
-
 class Block{
   contructor(index, timestamp, data, previousHash = ''){
     this.index = index;
@@ -11,30 +10,34 @@ class Block{
   }
 
   calculateHash(){
-return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
 
-
 class Blockchain{
   constructor(){
-    this.chain = [this.createGenesisBlock()];
+    this.chain = [this.createGenesisBlock];
   }
 
   createGenesisBlock(){
-    return new Block(0, "04/12/2018", "Genesis Block");
+    return new Block(0, "01/01/2018", "Genesis Block", "0");
   }
+
   getLatestBlock(){
     return this.chain[this.chain.length - 1];
   }
+
   addBlock(newBlock){
-    newBlock.previousHash = newBlock.calculateHash();
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
 }
 
-let aliCoin = new Blockchain();
-aliCoin.addBlock(new Block(1, "02/12/2018", { amount: 3 }));
-aliCoin.addBlock(new Block(2, "03/12/2018", { amount: 5 }));
+let daMoney = new Blockchain();
+daMoney.addBlock(new Block(1, "02/01/2018", { amound: 4 }));
+daMoney.addBlock(new Block(2, "03/01/2018", { amount: 10 }));
 
-console.log(JSON.stringify(aliCoin, null, 3))
+console.log(JSON.stringify(daMoney, null, 4));
+
+
